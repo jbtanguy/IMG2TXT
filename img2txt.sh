8817fe030f2014e58325e5cf46e8d0cd145e15d2
jbtanguy <jbtanguy56@gmail.com>
2021-03-11 09:51:33 +0100

    gestion des formats multiples en entrée

---
+++ img2txt.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+# Prérequis :
+# pdftoppm
+# python3
+# virtualenv
+
+inType=$1 # -p (PDF) -j (JPG) -t (TIF)
+outType=$2 # -t (TXT) -h (HTML)
+
+# 1. Environnement virtuel
+if [ ! -d "./venv_kraken/" ]; then
+	echo "Création de l'environnment virtuel pour accueillir Kraken."
+	mkdir ./venv_kraken/
+	virtualenv -p python3 venv_kraken
+	. ./venv_kraken/bin/activate
+	echo "Environnement virtuel venv_kraken créé.\n\nTéléchargement de Kraken."
+	pip3 install kraken
+else
+	. ./venv_kraken/bin/activate
+fi
+
+# 2. Modèle d'OCR
+# Simon Gabay, Thibault Clérice, Christian Reul. OCR17: Ground Truth and Models for 17th c. French Prints (and hopefully more). 2020. ⟨hal-02577236⟩
+# if [ ! -f "./CORPUS17.mlmodel" ]; then
+# 	echo "Téléchargement du modèle d'OCR."
+# 	wget https://zenodo.org/record/3826894/files/OCR17.zip?download=1 -O CORPUS17.mlmodel
+# fi
+
+# 3. Segmentation des pages
+if [ $inType = "-p" ]; then
+	for file in `ls ./data/*.pdf`; do pdftoppm -png $file $file; done
+fi
+
+# 4. Binarisation des images
+if [ $inType = "-p" ]; then
+	for file in `ls ./data/*.png`; do kraken -i $file $file"_bin.png" binarize; done
+elif [ $inType = "-j" ]; then
+	for file in `ls ./data/*.jpg`; do kraken -i $file $file"_bin.png" binarize; done
+elif [ $inType = "-t" ]; then
+	for file in `ls ./data/*.tif`; do kraken -i $file $file"_bin.png" binarize; done
+fi
+
+# 5. Segmentation et OCR
+if [ $outType = "-t" ]; then
+	for file in `ls ./data/*_bin.png`; do kraken -i $file $file".txt" segment ocr -m ./CORPUS17.mlmodel; done
+else
+	for file in `ls ./data/*_bin.png`; do kraken -i $file $file".html" segment ocr -m ./CORPUS17.mlmodel -h; done
+fi
+
+# 6. Rangement des fichiers de sorties
+# Suppression des outputs des processus antérieurs
+if [ -d "./out/" ]; then
+	rm -R ./out/
+fi
+# Création des dossiers pour les output
+mkdir ./out/
+if [ $inType = "-p" ]; then
+	mkdir ./out/pdf/
+elif [ $inType = "-j" ]; then
+	mkdir ./out/jpg/
+elif [ $inType = "-t" ]; then
+	mkdir ./out/tif/
+fi
+mkdir ./out/png/
+if [ $outType = "-h" ]; then
+	mkdir ./out/html/
+else
+	mkdir ./out/txt/
+fi
+cd ./data/
+# PDF
+if [ $inType = "-p" ]; then
+	for file in `ls *.pdf`; do mv $file ./../out/pdf/$file; done
+elif [ $inType = "-j" ]; then
+	for file in `ls *.jpg`; do mv $file ./../out/jpg/$file; done
+elif [ $inType = "-t" ]; then
+	for file in `ls *.tif`; do mv $file ./../out/tif/$file; done
+fi
+# PNG 
+for file in `ls *.png`; do mv $file ./../out/png/$file; done
+# HTML
+if [ $outType = "-h" ]; then
+	for file in `ls *.html`; do mv $file ./../out/html/$file; done
+else # TXT
+	for file in `ls *.txt`; do mv $file ./../out/txt/$file; done
+fi
