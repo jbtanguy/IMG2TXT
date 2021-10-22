0ad05715fc9ab8c70aee900d79127783debc9710
Orion Alié <someonefefe@gmail.com>
2021-10-22 14:00:21 +0200

    Perform all operations from script directory

--- img2txt.sh
+++ img2txt.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Prérequis :
 # pdftoppm
 # python3
@@ -9,29 +9,30 @@ outType=$2 # -t (TXT) -h (HTML)
 engine=$3 # -k (KRAKEN), -t (TESSERACT)
 dir_path=$4
 
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # 1. Environnement virtuel
 if [ $engine = "-k" ]; then
-	if [ ! -d "./venv_kraken/" ]; then
+	if [ ! -d $SCRIPT_DIR/venv_kraken/ ]; then
 		echo "Création de l'environnment virtuel pour accueillir Kraken."
-		mkdir ./venv_kraken/
-		python3 -m venv ./venv_kraken/
-		. ./venv_kraken/bin/activate
+		mkdir $SCRIPT_DIR/venv_kraken/
+		python3 -m venv $SCRIPT_DIR/venv_kraken/
+		. $SCRIPT_DIR/venv_kraken/bin/activate
 		echo "Environnement virtuel venv_kraken créé.\n\nTéléchargement de Kraken."
 		pip3 install kraken
 	else
-		. ./venv_kraken/bin/activate
+		. $SCRIPT_DIR/venv_kraken/bin/activate
 	fi
 elif [ $engine = "-t" ]; then
-	if [ ! -d "./venv_tesseract/" ]; then
+	if [ ! -d $SCRIPT_DIR/venv_tesseract/ ]; then
 		echo "Création de l'environnment virtuel pour accueillir Tesseract."
-		mkdir ./venv_tesseract/
+		mkdir $SCRIPT_DIR/venv_tesseract/
 		virtualenv -p python3 venv_tesseract
-		. ./venv_tesseract/bin/activate
+		. $SCRIPT_DIR/venv_tesseract/bin/activate
 		echo "Environnement virtuel venv_tesseract créé.\n\nTéléchargement de Tesseract."
 		pip3 install pytesseract opencv-python
 	else
-		. ./venv_tesseract/bin/activate
+		. $SCRIPT_DIR/venv_tesseract/bin/activate
 	fi
 fi
 
@@ -56,9 +57,9 @@ fi
 # 5. Segmentation et OCR
 if [ $engine = "-k" ]; then
 	if [ $outType = "-t" ]; then
-		for file in `ls $dir_path/*_bin.png`; do timeout 600 kraken -i $file $file".txt" segment ocr -m ./CORPUS17.mlmodel; done
+		for file in `ls $dir_path/*_bin.png`; do timeout 600 kraken -i $file $file".txt" segment ocr -m $SCRIPT_DIR/CORPUS17.mlmodel; done
 	else
-		for file in `ls $dir_path/*_bin.png`; do timeout 600 kraken -i $file $file".html" segment ocr -m ./CORPUS17.mlmodel -h; done
+		for file in `ls $dir_path/*_bin.png`; do timeout 600 kraken -i $file $file".html" segment ocr -m $SCRIPT_DIR/CORPUS17.mlmodel -h; done
 	fi
 elif [ $engine = "-t" ]; then
 	# code pour lancer tesseract avec un fichier de config
